1abf3a9814bae2f26d5abbb02b627b051ce1dea8
k-kashio <k-kashio@KFD103.kfad.internal>
2026-09-18 17:34:29 +0900

    add:Kashio Case01.java ログイン画面への遷移テストコード追加 WebElementを利用してタイトルの値とログインボタンの値を取得し、それぞれ期 待値通りかどうか確認するテストを作成いた。 仕様書の通りのクラスに納めれていなかったので追加。 期待値は以下の通り タイトル：ログイン ログインボタン：ログイン
    add:Kashio Case02.java受講生 ログイン 認証失敗 テストコード追加
    
    WebElementを利用してログインIDとパスワードを入力し、ログインボタンを押下した後、エラーメッセージとタイトルがそれぞれ期待値通りかどうか確認するテストを作成した。
    仕様書の通りのクラスに納めれていなかったので追加。
    期待値は以下の通り
    エラーメッセージ：* ログインに失敗しました。
    タイトル：ログイン | LMS
    
    add:Kashio evidenceの追加
    
    evidenceの追加を実装できていなかったので
    
    fix:Kashio テスト仕様書 テスト仕様書の表紙シートに更新履歴が追記 ケース02のテスト項目追記。
    
    指摘箇所の修正

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case02.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case02.java
@@ -1,6 +1,7 @@
 package jp.co.sss.lms.ct.f01_login1;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +10,8 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 /**
  * 結合テスト ログイン機能①
@@ -36,6 +39,12 @@ public class Case02 {
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
 		// TODO ここに追加
+		webDriver.get("http://localhost:8080/lms/");
+
+		assertEquals("http://localhost:8080/lms/", webDriver.getCurrentUrl());
+
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
@@ -43,6 +52,16 @@ public class Case02 {
 	@DisplayName("テスト02 DBに登録されていないユーザーでログイン")
 	void test02() {
 		// TODO ここに追加
+		webDriver.findElement(By.id("loginId")).sendKeys("kashiokoki");
+		webDriver.findElement(By.id("password")).sendKeys("kashiokoki");
+
+		WebElement loginButton = webDriver.findElement(By.cssSelector(".btn.btn-primary"));
+		loginButton.click();
+		WebElement errorMessage = webDriver.findElement(By.cssSelector("span.error"));
+		assertEquals("* ログインに失敗しました。", errorMessage.getText());
+		assertEquals("ログイン | LMS", webDriver.getTitle());
+		getEvidence(new Object() {
+		});
 	}
 
 }
